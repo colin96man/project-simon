62c56f4085f19d7f6069adbabced47bbb0267d0f
Colin Nelson <colinnelson1996@gmail.com>
2020-05-06 17:57:57 -0600

    pseudocode update 1

--- js/main.js
+++ js/main.js
@@ -3,5 +3,6 @@
 //write event listner for user click on buttons
 //create randomization function for lighting up triangles
 //create asynchronous timing for the triangles
+//create initialize function & game start button
 //render board when triangles are selcted
 //create "game over" logic and show score for how long the user played
