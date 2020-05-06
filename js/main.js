946252476713975089b53fd6225c26a23f58aa7e
Colin Nelson <colinnelson1996@gmail.com>
2020-05-06 17:57:10 -0600

    pseudocode in js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//create variable for each triangle/button
+//create board
+//write event listner for user click on buttons
+//create randomization function for lighting up triangles
+//create asynchronous timing for the triangles
+//render board when triangles are selcted
+//create "game over" logic and show score for how long the user played
