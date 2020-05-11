f5234a0cece407fd258c248ad45748ea16541b08
Colin Nelson <colinnelson1996@gmail.com>
2020-05-11 10:56:31 -0600

    random picker working

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-//Constants:
-    //buttons:
-        //game start button & the four clickable game buttons
-        //each triangle will be assigned a number
-    //board:
-        //grid square with four colored triangles, not lighting up
+const triangles = {
+    top: 'tomato',
+    left: 'cyan', 
+    right: 'lime',
+    bottom: 'lightyellow'
+};
 
-const redTri = 1;
-const blueTri = 2;
-const grnTri = 3;
-const ylwTri = 4;
-const board = [redTri, blueTri, grnTri, ylwTri];
+const board = [1, 2, 3, 4];
+
+const triangleEl = {
+    top: document.querySelector('#arrow-down'),
+    left: document.querySelector('#arrow-left'),
+    right: document.querySelector('#arrow-right'),
+    bottom: document.querySelector('#arrow-up')
+};
 
 //State variable:    
     //game over prompt with how many correct button pushes the player had
@@ -23,17 +26,16 @@ let roundNum;
 let simonsPattern;
 let playersPattern;
 
-
-//Event listeners:
-    //create event listener for clicking the triangle divs, which will "light up" the traingle
-    //create event listener for clicking the reset button which will call init
-    //create event listener for clicking the game start button, calling the function to start the first round
-
 document.getElementById('reset-btn').addEventListener('click', init);
 
+document.getElementById('start-btn').addEventListener('click', round);
+
 document.getElementById('arrow-down').addEventListener('click', playerClickRed);
+
 document.getElementById('arrow-left').addEventListener('click', playerClickBlue);
+
 document.getElementById('arrow-right').addEventListener('click', playerClickGrn);
+
 document.getElementById('arrow-up').addEventListener('click', playerClickYlw);
 
 
@@ -48,13 +50,13 @@ document.getElementById('arrow-up').addEventListener('click', playerClickYlw);
 init()
 
 function init() {
-    console.log('click');
+    //console.log('click');
     let simonsPattern = [];
     let playersPattern = [];
     let winner = null;
     let gameOver = null;
     let roundNum = 0;
-    render()
+    //render()
 }
 
 //Render:
@@ -64,35 +66,30 @@ function init() {
     //after round 10 check for win
 
 function render () {
-    
-}
-
-//renderBoard:
-    //if game has not been started or a round is not running then styling should not change
-    //for each triangle chosen change the styling to show it "lighting up"
-
-function renderBoard() {
 
 }
 
-//create a function that will randomly select triangles using Math
-
-function randomPick() {
-
+function randomPick(triangles) {
+    let trianglesKeys = Object.keys(triangles);
+    return triangles[trianglesKeys[trianglesKeys.length * Math.random() << 0]];
 }
 
 //create function for the asynchronus timing of how long a triangle will be "lit up"
     //each triangle's style will change and then return to normal in the same amount of time
 
-function timing() {
-
+function timing(triangle) {
+    setTimeout(function() {
+        triangleEl.style.backgroundColor = triangles[triangle];
+    }, 2000);
 }
 
 //create functions for the different rounds:
     //each round will increment up by one on how many times simon chooses a random triangle 
     //render
 
-
+function round() {
+    console.log('start button is working');
+}
 
 //create function for the player click event:
     //clicking one of the triangles will change the style just like when a random triangle is selected
@@ -104,23 +101,22 @@ function playerClick() {
     playerClickBlue()
     playerClickGrn()
     playerClickYlw()
+    //render()
 }
 
-function playerClickRed() {
-    console.log('top clicked')
+function playerClickRed(top) {
+    console.log('top clicked');
+    timing();
 }
 
 function playerClickBlue() {
-    console.log('clicked left')
+    console.log('clicked left');
 }
 
 function playerClickGrn() {
-    console.log('clicked right')
+    console.log('clicked right');
 }
 
 function playerClickYlw() {
-    console.log('clicked bottom')
+    console.log('clicked bottom');
 }
-
-//create functions for rounds
-    //call renderBoard after each one so that it will update the DOM
